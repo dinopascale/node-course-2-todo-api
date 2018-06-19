3989c7ce5b0ec8b23cea5f07b9439f547bab8aa2
dinopascale <dino.pascale@gmail.com>
2018-06-19 16:58:59 +0200

    Add tests for GET /todos

--- server/server.js
+++ server/server.js
@@ -23,8 +23,8 @@ app.get('/todos', (req, res) => {
     Todo.find().then(
         todos => { res.send({todos}) },
         e => { res.status(400).send(e) }
-    )
-})
+    );
+});
 
 app.listen(3000, () => { console.log(`Start on port 3000`) })
 
