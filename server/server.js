a6a2d27e6388a3d0ca52e7e3c1f4db6a94efe96e
dinopascale <dino.pascale@gmail.com>
2018-06-20 10:36:14 +0200

    Test the DELETE /todos/:id route

--- server/server.js
+++ server/server.js
@@ -55,7 +55,7 @@ app.delete('/todos/:id', (req, res) => {
         .then(todo => {
             if (!todo) { res.status(404).send() }
 
-            res.send(todo)
+            res.send({todo})
         })
         .catch(e => res.status(400).send())
 })
