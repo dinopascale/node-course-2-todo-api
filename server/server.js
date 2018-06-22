f871b3334705533f71aa3a0bb40b726296db9f05
dinopascale <dino.pascale@gmail.com>
2018-06-22 09:27:44 +0200

    Add POST /users/login route

--- server/server.js
+++ server/server.js
@@ -4,8 +4,9 @@ const _ = require('lodash');
 const express = require('express');
 const bodyParser = require('body-parser');
 const { ObjectID } = require('mongodb');
-
+const bcrypt = require('bcryptjs');
 const { mongoose } = require('./db/mongoose');
+
 const { Todo } = require('./models/todo');
 const { User } = require('./models/user');
 const { authenticate } = require('./middleware/authenticate');
@@ -107,9 +108,25 @@ app.post('/users', (req, res) => {
         )
 })
 
-
 app.get('/users/me', authenticate, (req, res) => {
     res.send(req.user);
+})
+
+app.post('/users/login', (req, res) => {
+    const body = _.pick(req.body, ['email', 'password']);
+    let utent;
+
+    User.findByCredentials(body.email, body.password)
+        .then((user) => {
+            utent = user; 
+            return user.generateAuthToken();
+        })
+        .then((token) => {
+            res.header('x-auth', token).send(utent);
+        })
+        .catch(
+            err => res.status(400).send()
+        )
 })
 
 app.listen(port, () => { console.log(`Start on port ${port}`) })
